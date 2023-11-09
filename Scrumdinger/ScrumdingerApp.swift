655df7afc7029d1c8e00afb77731d6976a24a48f
Beau Rubin-Rottenberg <beaurubrot@gmail.com>
2023-11-08 21:40:40 -0500

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Beau Rubin-Rottenberg on 11/8/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
